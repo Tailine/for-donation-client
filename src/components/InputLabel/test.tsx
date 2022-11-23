6d2e8a3f3e0bf5ad037caa01dc6932bd43660210
Tailine <tailinechagas@gmail.com>
2022-11-23 11:52:09 -0300

    test: changes on input and initial value passed to it

--- src/components/InputLabel/test.tsx
+++ src/components/InputLabel/test.tsx
@@ -1,30 +1,66 @@
-import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { render, screen, waitFor } from '@testing-library/react'
 import { InputLabel } from '.'
 
+type Args = Partial<{
+  onValueChange(): void
+  isInvalid: boolean
+  errorMessage: string
+  initialValue: string
+}>
+
+function renderComponent({
+  initialValue,
+  onValueChange,
+  isInvalid = false,
+  errorMessage
+}: Args) {
+  return render(
+    <InputLabel
+      labelProps={{ htmlFor: 'name', labelText: 'Name:' }}
+      inputProps={{
+        id: 'name',
+        name: 'name',
+        onValueChange: onValueChange ?? jest.fn(),
+        initialValue
+      }}
+      formControlProps={{ isInvalid }}
+      errorMessage={errorMessage}
+    />
+  )
+}
+
 describe('<InputLabel />', () => {
-  test('renders correctly', () => {
-    render(
-      <InputLabel
-        labelProps={{ htmlFor: 'name', labelText: 'Name:' }}
-        inputProps={{ id: 'name', name: 'name' }}
-      />
-    )
+  it('renders correctly', () => {
+    renderComponent({})
 
     expect(screen.getByRole('textbox')).toBeInTheDocument()
     expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
     expect(screen.getByText(/name/i)).toBeInTheDocument()
   })
 
-  test('should render error message for invalid input', () => {
-    render(
-      <InputLabel
-        labelProps={{ htmlFor: 'name', labelText: 'Name:' }}
-        inputProps={{ id: 'name', name: 'name' }}
-        formControlProps={{ isInvalid: true }}
-        errorMessage="Something is wrong"
-      />
-    )
+  it('should render error message for invalid input', () => {
+    renderComponent({ errorMessage: 'Something is wrong' })
 
     expect(screen.getByText(/something is wrong/i)).toBeInTheDocument()
+  })
+
+  it('should call onValueChange func on input change', async () => {
+    const changeFn = jest.fn()
+    renderComponent({ onValueChange: changeFn })
+
+    userEvent.type(screen.getByRole('textbox'), 'Maria')
+
+    await waitFor(() => {
+      expect(screen.getByRole('textbox')).toHaveValue('Maria')
+      expect(changeFn).toHaveBeenCalledWith('Maria')
+      expect(changeFn).toHaveBeenCalled()
+    })
+  })
+
+  it('should render input with initialValue', () => {
+    renderComponent({ initialValue: 'Pedro' })
+
+    expect(screen.getByRole('textbox')).toHaveValue('Pedro')
   })
 })
