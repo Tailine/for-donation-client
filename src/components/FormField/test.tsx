6b66dae00d2d9af5790070568441194b0ad9e32b
Tailine <tailinechagas@gmail.com>
2022-12-01 19:44:47 -0300

    chore: encapsulates repeated logic from fields

---
+++ src/components/FormField/test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react'
+import { FormField, Props as FormFieldProps } from '.'
+
+type Args = Omit<FormFieldProps, 'labelProps' | 'children'>
+
+function renderComponent({ errorMessage, formControlProps }: Args) {
+  return render(
+    <FormField
+      labelProps={{ htmlFor: 'category', labelText: 'Categoria' }}
+      errorMessage={errorMessage}
+      formControlProps={formControlProps}
+    >
+      <input id="category" />
+    </FormField>
+  )
+}
+
+describe('<FormField />', () => {
+  it('should render correctly', () => {
+    renderComponent({})
+
+    screen.debug()
+    expect(screen.getByLabelText(/categoria/i)).toBeInTheDocument()
+    expect(screen.getByText(/categoria/i)).toBeInTheDocument()
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+
+  it('should render error message for invalid input', () => {
+    renderComponent({
+      errorMessage: 'Something is wrong',
+      formControlProps: { isInvalid: true }
+    })
+
+    expect(screen.getByText(/something is wrong/i)).toBeInTheDocument()
+  })
+})
