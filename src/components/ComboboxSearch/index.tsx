cc5abaaa4b1b1408494daac5e889cea2e5d0a9e5
Tailine <tailinechagas@gmail.com>
2022-12-26 19:46:33 -0300

    fix: display error border when input is invalid

--- src/components/ComboboxSearch/index.tsx
+++ src/components/ComboboxSearch/index.tsx
@@ -7,42 +7,63 @@ type Props = {
   name: string
   onChange(value: string): void
   placeholder?: string
+  isInvalid?: boolean
+  isDisabled?: boolean
+  isLoading?: boolean
+  ariaLabel?: string
 }
 
-const styles: StylesConfig = {
-  control: (css, props) => {
-    return {
+function getBorderColor(
+  isFocused: boolean,
+  isDisabled: boolean,
+  isInvalid: boolean
+) {
+  if (isInvalid) return '#B44B4B'
+  if (isDisabled) return '#dcdcdc'
+  if (isFocused) return '#293D39'
+  return '#718096'
+}
+
+function stylesOverride(isInvalid: boolean): StylesConfig {
+  return {
+    control: (css, props) => {
+      return {
+        ...css,
+        borderColor: getBorderColor(
+          props.isFocused,
+          props.isDisabled,
+          isInvalid
+        ),
+        backgroundColor: props.isDisabled ? '#F0F5F4' : 'transparent',
+        borderRadius: 0,
+        boxShadow: isInvalid ? '0 0 0 1px #B44B4B' : 'none',
+        borderWidth: props.isFocused ? 2 : 1,
+        ':hover': { borderColor: '#293D39' }
+      }
+    },
+    input: (css) => ({
       ...css,
-      borderColor: props.isFocused ? '#293D39' : '#718096',
-      backgroundColor: 'transparent',
-      borderRadius: 0,
-      boxShadow: 'none',
-      borderWidth: props.isFocused ? 2 : 1,
+      borderColor: '#293D39'
+    }),
+    option: (css, props) => ({
+      ...css,
+      backgroundColor: props.isSelected
+        ? '#9EBDB8'
+        : props.isFocused
+        ? '#D5E2E0'
+        : 'unset',
+      ':active': { backgroundColor: '#9EBDB8' },
       ':hover': { borderColor: '#293D39' }
-    }
-  },
-  input: (css) => ({
-    ...css,
-    borderColor: '#293D39'
-  }),
-  option: (css, props) => ({
-    ...css,
-    backgroundColor: props.isSelected
-      ? '#9EBDB8'
-      : props.isFocused
-      ? '#D5E2E0'
-      : 'unset',
-    ':active': { backgroundColor: '#9EBDB8' },
-    ':hover': { borderColor: '#293D39' }
-  }),
-  indicatorSeparator: (css) => ({
-    ...css,
-    display: 'none'
-  }),
-  dropdownIndicator: (css) => ({
-    ...css,
-    color: '#293D39'
-  })
+    }),
+    indicatorSeparator: (css) => ({
+      ...css,
+      display: 'none'
+    }),
+    dropdownIndicator: (css) => ({
+      ...css,
+      color: '#293D39'
+    })
+  }
 }
 
 export function ComboboxSearch({
@@ -50,19 +71,27 @@ export function ComboboxSearch({
   onChange,
   name,
   id,
-  placeholder = 'Selecione uma opção'
+  placeholder = 'Selecione uma opção',
+  isDisabled = false,
+  isLoading = false,
+  isInvalid = false,
+  ariaLabel
 }: Props) {
   return (
     <ReactSelect
+      data-testid="select-city"
       instanceId={id}
+      isDisabled={isDisabled}
+      isLoading={isLoading}
       id={id}
+      aria-label={ariaLabel}
       name={name}
       onChange={(selected: { label: string; value: string }) =>
         onChange(selected.value)
       }
       options={options}
       placeholder={placeholder}
-      styles={styles}
+      styles={stylesOverride(isInvalid)}
       filterOption={(option, input) => {
         return formatSearchString(option.label).includes(
           formatSearchString(input)
