0a55c85abd1497ddc84c67f6d17ccdb5ec83a239
Tailine <tailinechagas@gmail.com>
2023-01-11 18:08:08 -0300

    feat: update font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { customTheme } from 'config/theme'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Bebas_Neue } from '@next/font/google'
 
+const bebas = Bebas_Neue({ weight: '400', subsets: ['latin'] })
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={customTheme}>
-        <Component {...pageProps} />
+        <main className={bebas.className}>
+          <Component {...pageProps} />
+        </main>
       </ChakraProvider>
     </QueryClientProvider>
   )
