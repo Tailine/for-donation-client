9f9caa9b5a21db38f2ba13ddcfa36532e598a4e5
Tailine <tailinechagas@gmail.com>
2023-01-12 20:18:18 -0300

    refactor: change font and styles

--- src/components/DonationCard/index.tsx
+++ src/components/DonationCard/index.tsx
@@ -1,8 +1,14 @@
-import { Box, Flex, Heading, Tag, TagLabel, Text } from '@chakra-ui/react'
+import { Box, Flex, Link, Tag, TagLabel, Text } from '@chakra-ui/react'
 import { Button } from 'components/Button'
+import { Heading } from 'components/Heading'
 import { Modal } from 'components/Modal'
 import Image from 'next/image'
 import { useState } from 'react'
+import { Raleway } from '@next/font/google'
+import { Location } from 'components/Icons/Location'
+import NextLink from 'next/link'
+
+const raleway = Raleway({ weight: '500', subsets: ['latin'] })
 
 export type Props = {
   id: string
@@ -17,6 +23,10 @@ export type Props = {
     onEdit(id: string): void
     onDelete(id: string): void
   }
+  location: {
+    city: string
+    state: string
+  }
 }
 
 export function DonationCard({
@@ -25,6 +35,7 @@ export function DonationCard({
   description,
   image,
   title,
+  location,
   controls
 }: Props) {
   const [isModalVisible, setIsModalVisible] = useState(false)
@@ -35,82 +46,110 @@ export function DonationCard({
   }
 
   return (
-    <Box
+    <Link
       maxWidth={400}
-      backgroundColor="yellow.50"
-      paddingY={6}
-      paddingX={4}
-      border="1px"
-      borderColor="purple.700"
+      as={NextLink}
+      href={`doacao/${id}`}
+      textDecoration="none"
+      _hover={{
+        textDecoration: 'none',
+        transform: 'translate(0, -10px)',
+        boxShadow: '5px 5px rgb(76,77,128)'
+      }}
     >
-      <Box position="relative">
-        <Image width={500} height={300} alt={image.alt} src={image.url} />
-        <Tag
-          right={2}
-          bottom={4}
-          position="absolute"
-          backgroundColor="yellow.100"
-          border="2px"
-          borderRadius={0}
-          borderColor="purple.700"
-        >
-          <TagLabel
-            paddingY={{ base: 2, md: 2 }}
-            paddingX={{ base: 0, md: 2 }}
-            color="purple.700"
-            fontWeight="bold"
-            fontSize={{ base: 'x-small', md: 'small' }}
+      <Box height="100%" backgroundColor="yellow.50" paddingY={6} paddingX={4}>
+        <Box position="relative">
+          <Box
+            position="relative"
+            height={{ base: 120, md: 150 }}
+            width={{ base: '100%' }}
+            marginBottom={4}
           >
-            {category}
-          </TagLabel>
-        </Tag>
-      </Box>
-      <Box>
-        <Heading color="purple.700" marginY={4} size="md">
-          {title}
-        </Heading>
-        <Text color="gray.600" fontSize="sm">
-          {description}
-        </Text>
-      </Box>
-      {controls && (
-        <Flex marginTop={4} justifyContent="end">
-          <Button size="sm" variant="ghost" onClick={() => controls.onEdit(id)}>
-            Editar
-          </Button>
-          <Button
-            size="sm"
-            variant="ghost"
-            marginLeft={2}
-            color="red.500"
-            colorScheme="red"
-            fontSize="small"
-            onClick={() => setIsModalVisible(true)}
-          >
-            Deletar
-          </Button>
-        </Flex>
-      )}
-      <Modal
-        title="Excluir doação"
-        content={<Text>Tem certeza que deseja excluir essa doação?</Text>}
-        isOpen={isModalVisible}
-        footer={
-          <Box marginTop={4}>
-            <Button
-              variant="outlined"
-              colorScheme="green"
-              marginRight={6}
-              onClick={() => setIsModalVisible(false)}
-            >
-              Cancelar
-            </Button>
-            <Button variant="solid" onClick={handleConfirm}>
-              Confirmar
-            </Button>
+            {' '}
+            <Image alt={image.alt} src={image.url} fill />
           </Box>
-        }
-      />
-    </Box>
+          <Tag
+            right={2}
+            bottom={4}
+            position="absolute"
+            backgroundColor="purple.200"
+            border="2px"
+            borderRadius={0}
+            borderColor="purple.700"
+          >
+            <TagLabel
+              paddingY={{ base: 2, md: 2 }}
+              paddingX={{ base: 0, md: 2 }}
+              color="purple.700"
+              fontWeight="bold"
+              fontSize={{ base: 'x-small', md: 'small' }}
+            >
+              {category}
+            </TagLabel>
+          </Tag>
+        </Box>
+        <Box fontFamily="inherit">
+          <Heading marginY={4} size="md">
+            {title}
+          </Heading>
+          <Flex marginY={4}>
+            <Location />
+            <Text
+              marginLeft={2}
+              color="gray.500"
+              fontSize="xs"
+              className={raleway.className}
+            >
+              {location.city} - {location.state}
+            </Text>
+          </Flex>
+          <Text color="black" fontSize="sm" className={raleway.className}>
+            {description}
+          </Text>
+        </Box>
+        {controls && (
+          <Flex marginTop={4} justifyContent="end">
+            <Button
+              size="sm"
+              variant="ghost"
+              onClick={() => controls.onEdit(id)}
+            >
+              Editar
+            </Button>
+            <Button
+              size="sm"
+              variant="ghost"
+              marginLeft={2}
+              color="red.500"
+              colorScheme="red"
+              fontSize="small"
+              onClick={() => setIsModalVisible(true)}
+            >
+              Deletar
+            </Button>
+          </Flex>
+        )}
+        <Modal
+          title="Excluir doação"
+          content={<Text>Tem certeza que deseja excluir essa doação?</Text>}
+          isOpen={isModalVisible}
+          footer={
+            <Box marginTop={4}>
+              <Button
+                variant="outlined"
+                colorScheme="green"
+                marginRight={6}
+                onClick={() => setIsModalVisible(false)}
+              >
+                Cancelar
+              </Button>
+              <Button variant="solid" onClick={handleConfirm}>
+                Confirmar
+              </Button>
+            </Box>
+          }
+        />
+      </Box>
+    </Link>
   )
 }
