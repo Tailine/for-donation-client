9e83d021242fa33644865561a1cc0c9e6fbeec5e
Tailine <tailinechagas@gmail.com>
2023-01-21 21:50:55 -0300

    feat: replace content with children

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -11,19 +11,19 @@ import { ReactNode } from 'react'
 
 export type Props = {
   title: string
-  content: ReactNode
+  children: ReactNode
   isOpen: boolean
   footer?: JSX.Element
 }
 
-export function Modal({ title, content, isOpen, footer }: Props) {
+export function Modal({ title, children, isOpen, footer }: Props) {
   const { onClose } = useDisclosure()
   return (
     <ChackraModal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent borderRadius={0}>
         <ModalHeader>{title}</ModalHeader>
-        <ModalBody>{content}</ModalBody>
+        <ModalBody>{children}</ModalBody>
 
         <ModalFooter>{footer}</ModalFooter>
       </ModalContent>
